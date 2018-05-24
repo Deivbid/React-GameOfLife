6e33c1c532d8b24974ebf09dd7129e3d873ada44
David <davidaapariciobak@gmail.com>
2018-05-24 17:31:16 -0400

    Grid progress, think the logic

---
+++ src/components/cell/Cell.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Cell extends Component {
+    
+    render() { 
+        const { x, y } = this.props;
+        return ( 
+            <div className="Cell" style={{
+                left: `${this.props.size * x + 1}px`,
+                top: `${this.props.size * y + 1}px`,
+                width: `${this.props.size - 1}px`,
+                height: `${this.props.size - 1}px`,
+              }} />
+         )
+    }
+}
+ 
+export default Cell;
